618e6901e8e25d1c9d3cb52567ecdd9c5870e445
aguillo2024 <guillo.antoine93@gmail.com>
2025-08-26 09:28:21 +0200

    AJout logging dans la classe Service response

--- src/main/java/tp/eni_store/service/ServiceHelper.java
+++ src/main/java/tp/eni_store/service/ServiceHelper.java
@@ -1,6 +1,12 @@
 package tp.eni_store.service;
 
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+
 public class ServiceHelper {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceHelper.class);
 
     static <T> ServiceResponse<T> buildResponse(String code, String message, T data) {
 
@@ -9,7 +15,7 @@ public class ServiceHelper {
         serviceResponse.message = message;
         serviceResponse.data = data;
 
-        System.out.println(serviceResponse);
+        logger.info(String.format("[%s] - [%s]", code, message));
 
         return serviceResponse;
     }
